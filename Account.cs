29d8fa5f5b235b089d37e5e184525a1fb7d61ccb
Andrii543 <andrii.rusyn15@gmail.com>
2024-12-20 12:10:14 +0100

    Bank Account

---
+++ Account.cs
@@ -0,0 +1,88 @@
+﻿namespace Home_Work_14._1
+{
+    public class Account
+    {
+        private string bankAccount;
+
+        private decimal balance;
+
+        private string name;
+
+        public string BankAccount
+        {
+            get
+            {
+                return bankAccount;
+            }
+            set
+            {
+                bankAccount = value;
+            }
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+
+        public Account(string bankAccount, decimal balance)
+        {
+            this.balance = balance;
+            this.bankAccount = bankAccount;
+        }
+
+        public void HelloUser(string name)
+        {
+            Console.WriteLine($"\nВітаю {name} в банку {bankAccount}\n");
+        }
+
+
+        public decimal Withdrawal(decimal amount)
+        {
+            bool verification = true;
+            decimal result = 0;
+            while (verification)
+            {
+                if (amount > balance)
+                {
+                    Console.WriteLine("Заданна сума перевищує баланс на вашому рахунку");
+                    break;
+                }
+                result = balance - amount;
+
+                balance = result;
+                verification = false;
+
+            }
+            Console.WriteLine($"Коштів на вашому рахунку : {balance:F2}\n");
+            return result;
+        }
+
+        public decimal Deposit(decimal amount)
+        {
+            decimal result = amount + balance;
+            balance = result;
+
+            Console.WriteLine($"Коштів на вашому рахунку : {balance:F2}\n");
+            return result;
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"\nНа вашому рахунку: {balance}");
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+
+namespace Home_Work_14._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+
+            // 1 Bank 
+            bool exit = true;
+
+            while (exit)
+            {
+
+                Console.Write("Введіть назву свого банку: ");
+                string bankName = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(bankName) || bankName == "")
+                {
+                    Console.WriteLine("Назва банку не може бути пустою, спробуйте ще раз");
+                    continue;
+                }
+
+
+                Account accountUa = new Account(bankName, 100.00m);
+
+                Console.Write("Введіть своє ім'я: ");
+                string userName = Console.ReadLine();
+
+                accountUa.HelloUser(userName);
+
+                bool exitOperation = true;
+
+                while (exitOperation)
+                {
+                    Console.WriteLine("Виберіть операцію :\n1 - Перевірити баланас \n2 - Зняти кошти \n3 - Додати кошти \n4 - Вихід");
+                    int inputOperation = Convert.ToInt32(Console.ReadLine());
+
+
+
+                    switch (inputOperation)
+                    {
+                        case 1:
+                            accountUa.CheckBalance();
+                            break;
+
+                        case 2:
+                            Console.Write("\nВведіть суму для зняття коштів: ");
+                            decimal inputWithdrawal = Convert.ToDecimal(Console.ReadLine());
+                            accountUa.Withdrawal(inputWithdrawal);
+                            break;
+                        case 3:
+                            Console.Write("\nВведіть суму для додання коштів: ");
+                            decimal inputDeposit = Convert.ToDecimal(Console.ReadLine());
+                            accountUa.Deposit(inputDeposit);
+                            break;
+                        case 4:
+                            exit = false;
+                            exitOperation = false;
+                            break;
+                        default:
+                            Console.WriteLine("Не правельна вибрана операція");
+                            break;
+
+                    }
+                }
+  
+            }
+
+            // 2 Bank
+
+            Account accountPl = new Account("mBankPl", 234.23m);
+
+            accountPl.CheckBalance();
+
+            accountPl.Withdrawal(50);
+
+            accountPl.Deposit(50);
+
+        }
+    }
+}
